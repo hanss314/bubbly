a7b45308fa9334ca0eb35573607e92d7f554fb38
Bottersnike <bottersnike237@gmail.com>
2017-09-01 12:39:37 +0100

    Try this Tant

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ var bubble_time = 5000;
 var score = 0;
 var alive = false;
 var faded = false;
+var physics = false;
 
 var name = "";
 
@@ -20,62 +21,64 @@ var name = "";
 
     $("#score").text(score);
 
-    var i, j, position, velocity, position2, velocity2;
-    // Physics
+    if (physics) {
+        var i, j, position, velocity, position2, velocity2;
+        // Physics
 
-    // Move 'em
-    for (i = 0; i < bubbles.length; i += 1) {
-        position = bubbles[i].body.position;
-        velocity = bubbles[i].body.velocity;
+        // Move 'em
+        for (i = 0; i < bubbles.length; i += 1) {
+            position = bubbles[i].body.position;
+            velocity = bubbles[i].body.velocity;
 
-        position.x += velocity.x;
+            position.x += velocity.x;
 
-        if (position.x > 100 - DIAMETER / 2 || position.x < DIAMETER / 2) {
-            position.x -= velocity.x;
-            velocity.x = -velocity.x;
+            if (position.x > 100 - DIAMETER / 2 || position.x < DIAMETER / 2) {
+                position.x -= velocity.x;
+                velocity.x = -velocity.x;
+            }
+
+            position.y += velocity.y;
+
+            if (position.y > 100 - DIAMETER / 2 || position.y < DIAMETER / 2) {
+                position.y -= velocity.y;
+                velocity.y = -velocity.y;
+            }
         }
 
-        position.y += velocity.y;
+        // Collide 'em
+        for (i = 0; i < bubbles.length; i += 1) {
+            position = bubbles[i].body.position;
+            velocity = bubbles[i].body.velocity;
 
-        if (position.y > 100 - DIAMETER / 2 || position.y < DIAMETER / 2) {
-            position.y -= velocity.y;
-            velocity.y = -velocity.y;
-        }
-    }
+            for (j = 0; j < bubbles.length; j += 1) {
+                position2 = bubbles[j].body.position;
+                velocity2 = bubbles[j].body.velocity;
 
-    // Collide 'em
-    for (i = 0; i < bubbles.length; i += 1) {
-        position = bubbles[i].body.position;
-        velocity = bubbles[i].body.velocity;
+                if (position != position2) {
+                    if (position.x + position.radius + position2.radius > position2.x
+                        && position.x < position2.x + position.radius + position2.radius
+                        && position.y + position.radius + position2.radius > position2.y
+                        && position.y < position2.y + position.radius + position2.radius) {
 
-        for (j = 0; j < bubbles.length; j += 1) {
-            position2 = bubbles[j].body.position;
-            velocity2 = bubbles[j].body.velocity;
+                        var dist = Math.sqrt(((position.x - position2.x) * (position.x - position2.x))
+                            + ((position.y - position2.y) * (position.y - position2.y)));
+                        if (dist < position.radius + position2.radius) {
+                            // Collision.
+                            var newVelX1 = (velocity.x * (position.mass - position2.mass) + (2 * position2.mass * velocity2.x)) / (position.mass + position2.mass);
+                            var newVelY1 = (velocity.y * (position.mass - position2.mass) + (2 * position2.mass * velocity2.y)) / (position.mass + position2.mass);
+                            var newVelX2 = (velocity2.x * (position2.mass - position.mass) + (2 * position.mass * velocity.x)) / (position.mass + position2.mass);
+                            var newVelY2 = (velocity.y * (position2.mass - position.mass) + (2 * position.mass * velocity.y)) / (position.mass + position2.mass);
 
-            if (position != position2) {
-                if (position.x + position.radius + position2.radius > position2.x
-                    && position.x < position2.x + position.radius + position2.radius
-                    && position.y + position.radius + position2.radius > position2.y
-                    && position.y < position2.y + position.radius + position2.radius) {
+                            velocity.x = newVelX1;
+                            velocity.y = newVelY1;
+                            velocity2.x = newVelX2;
+                            velocity2.y = newVelY2;
 
-                    var dist = Math.sqrt(((position.x - position2.x) * (position.x - position2.x))
-                        + ((position.y - position2.y) * (position.y - position2.y)));
-                    if (dist < position.radius + position2.radius) {
-                        // Collision.
-                        var newVelX1 = (velocity.x * (position.mass - position2.mass) + (2 * position2.mass * velocity2.x)) / (position.mass + position2.mass);
-                        var newVelY1 = (velocity.y * (position.mass - position2.mass) + (2 * position2.mass * velocity2.y)) / (position.mass + position2.mass);
-                        var newVelX2 = (velocity2.x * (position2.mass - position.mass) + (2 * position.mass * velocity.x)) / (position.mass + position2.mass);
-                        var newVelY2 = (velocity.y * (position2.mass - position.mass) + (2 * position.mass * velocity.y)) / (position.mass + position2.mass);
-
-                        velocity.x = newVelX1;
-                        velocity.y = newVelY1;
-                        velocity2.x = newVelX2;
-                        velocity2.y = newVelY2;
-
-                        position.x += newVelX1;
-                        position.y += newVelY1;
-                        position2.x += newVelX2;
-                        position2.y += newVelY2;
+                            position.x += newVelX1;
+                            position.y += newVelY1;
+                            position2.x += newVelX2;
+                            position2.y += newVelY2;
+                        }
                     }
                 }
             }
@@ -238,51 +241,6 @@ function create_bubble() {
     }
 }
 
-$(document).on("touchstart mousedown", function (e) {
-    if (alive) {
-        var rel_x, rel_y, ga;
-        ga = $("#game_area");
-        rel_x = ((e.clientX - ga.position().left) / ga.width()) * 100;
-        rel_y = ((e.clientY - ga.position().top) / ga.height()) * 100;
-
-        var kill = [];
-        for (var i = 0; i < bubbles.length; i += 1) {
-            var position = bubbles[i].body.position;
-
-            if (position.x + position.radius > rel_x
-                && position.x < rel_x + position.radius
-                && position.y + position.radius > rel_y
-                && position.y < rel_y + position.radius) {
-
-                var dist = Math.sqrt(((position.x - rel_x) * (position.x - rel_x))
-                    + ((position.y - rel_y) * (position.y - rel_y)));
-                if (dist < position.radius) {
-                    // Collision.
-                    kill.push(bubbles[i]);
-                }
-            }
-        }
-
-        for (i = 0; i < kill.length; i ++) {
-            bubbles.splice(bubbles.indexOf(kill[i]), 1);
-
-            if (kill[i].state.type == "green") {
-                score++;
-                kill[i].dom.parent.remove();
-            } else if (kill[i].state.type == "red") {
-                trigger_death(kill[i]);
-            } else if (kill[i].state.type == "equation") {
-                if (kill[i].state.correct) {
-                    score++;
-                    kill[i].dom.parent.remove();
-                } else {
-                    trigger_death(kill[i]);
-                }
-            }
-        }
-    }
-});
-
 function trigger_death(bubble) {
     var dom_death = $("#death");
     var dom_restart = $("#restart-fade");
@@ -335,6 +293,23 @@ function trigger_death(bubble) {
         }
     );
     dom_death.show();
+}
+
+function readSingleFile(e) {
+    var file = e.target.files[0];
+    if (!file) {
+        return;
+    }
+    var reader = new FileReader();
+    reader.onload = function(e) {
+        var contents = e.target.result;
+        return contents;
+    };
+    reader.readAsText(file);
+}
+
+function load_osu() {
+    $("#file-loader").trigger('click');
 }
 
 $().ready(function () {
@@ -438,6 +413,54 @@ $().ready(function () {
                     }
                 );
                 dom_death.show();
+            }
+        }
+    });
+
+
+    $(document).on("touchstart mousedown", function (e) {
+        e.preventDefault();
+
+        if (alive) {
+            var rel_x, rel_y, ga;
+            ga = $("#game_area");
+            rel_x = ((e.clientX - ga.position().left) / ga.width()) * 100;
+            rel_y = ((e.clientY - ga.position().top) / ga.height()) * 100;
+
+            var kill = [];
+            for (var i = 0; i < bubbles.length; i += 1) {
+                var position = bubbles[i].body.position;
+
+                if (position.x + position.radius > rel_x
+                    && position.x < rel_x + position.radius
+                    && position.y + position.radius > rel_y
+                    && position.y < rel_y + position.radius) {
+
+                    var dist = Math.sqrt(((position.x - rel_x) * (position.x - rel_x))
+                        + ((position.y - rel_y) * (position.y - rel_y)));
+                    if (dist < position.radius) {
+                        // Collision.
+                        kill.push(bubbles[i]);
+                    }
+                }
+            }
+
+            for (i = 0; i < kill.length; i ++) {
+                bubbles.splice(bubbles.indexOf(kill[i]), 1);
+
+                if (kill[i].state.type == "green") {
+                    score++;
+                    kill[i].dom.parent.remove();
+                } else if (kill[i].state.type == "red") {
+                    trigger_death(kill[i]);
+                } else if (kill[i].state.type == "equation") {
+                    if (kill[i].state.correct) {
+                        score++;
+                        kill[i].dom.parent.remove();
+                    } else {
+                        trigger_death(kill[i]);
+                    }
+                }
             }
         }
     });
